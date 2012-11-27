7085ba7138d7acb5916396a777e117970a71eb14
Daniel <daniel.hernandez.zafra@gmail.com>
2012-11-27 19:55:05 +0100

    cambios para aañadir logica al paso de login, utilizando $.ajax

--- cauth.js
+++ cauth.js
@@ -1,29 +1,31 @@
 var connector = require('./cpersistence');
 var usersBBDD = {
-	"dani" : "calamar",
-	"bea"  : "guapisima"
+	'dani': 'calamar',
+	'bea': 'guapisima'
 };
 
 module.exports = function cauthSetup() {
 	return function cauthHandle(request, response, next) {
-		if(/^\/login\/?$/.test(request.url)) {
+		if (/^\/login\/?$/.test(request.url)) {
+			console.log('entra por login');
 			request.on('data', function(chunk) {
-				if(request.headers["content-type"] === 'application/json') {
+				if (request.headers['content-type'] === 'application/json') {
 					var objResult;
 					objResult = JSON.parse(chunk);
-					if(objResult["user"] && objResult["pass"]) {
-						if(request.method === "POST") {
-							if(usersBBDD[objResult["user"]] && usersBBDD[objResult["user"]] === objResult["pass"]) {
-								request.session.data.user = objResult["user"];
-								response.writeHead(200, {'content-type':'text/plain'});
-								response.end("Login correct");
+					if (objResult['user'] && objResult['pass']) {
+						if (request.method === 'POST') {
+							if (typeof objResult['user'] !== 'undefined' &&
+								usersBBDD[objResult['user']] === objResult['pass']) {
+								request.session.data.user = objResult['user'];
+								response.writeHead(200, {'content-type': 'text/plain'});
+								response.end('Login correct');
 							}
 							else {
-								response.writeHead(401, {'content-type':'text/plain'});
-			 					response.end("Login incorrect");
+								response.writeHead(401, {'content-type': 'text/plain'});
+								response.end('Login incorrect');
 							}
 						}
-						else if(request.method === "PUT") {
+						else if (request.method === "PUT") {
 							connector.checkUser(objResult["user"]);
 							console.log(checkUser);
 						}
